e1c3d668d922aa5b019a298aaa30978ba2a40ba8
yup <>
2022-05-17 21:45:23 +0600

    2
    2

--- my-app/src/components/post/Post.jsx
+++ my-app/src/components/post/Post.jsx
@@ -1,33 +1,37 @@
 import "./post.css"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {Users} from "../../dummyData"
 
-export default function Post() {
-  return (
+
+export default function Post({post}) {
+    return (
     <div className="post">
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img className="postProfileImg" 
-                    src="/assets/person/1.jpeg" alt=""/>
-                    <span className="postUsername">Ulan Mamont</span>
-                    <span className="postDate">5 min(s) ago</span>
+                    <img className="postProfileImg"
+                     src={Users.filter((u) => u.id === post?.userId)[0].profilePicture} alt=""/>
+                    <span className="postUsername">
+                        {Users.filter((u) => u.id === post?.userId)[0].username}
+                    </span>
+                    <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postTopRight">
                     <MoreVertIcon/>
                 </div>
             </div>
             <div className="postCenter">
-                <span className="postText">Hey! Its my first post</span>
-                <img className="postImg" src="assets/post/1.jpeg" alt="" />
+                <span className="postText">{post?.desc}</span>
+                <img className="postImg" src={post.photo} alt="" />
             </div>
             <div className="postBottom">
                 <div className="postButtonLeft">
                     <img className="likeIcon" src="assets/like.png" alt="" />
                     <img className="heartIcon" src="assets/heart.png" alt="" />
-                    <span className="postLikeCounter">11 people like it</span>
+                    <span className="postLikeCounter">{post.like} people like it</span>
                 </div>
                 <div className="postButtonRight">
-                    <span className="postCommentText">10 comments</span>
+                    <span className="postCommentText">{post.comment} comments</span>
                 </div>
             </div>
         </div>
